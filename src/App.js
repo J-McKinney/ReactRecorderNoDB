e65249dee62106b044ccb107e435e80afa4c615b
Jesse McKinney <jessman51386@gmail.com>
2020-03-26 15:04:10 -0400

    yay

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Recorder from "./pages/Recorder/Recorder";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Recorder />
+    </>
+  );
+}
+
+export default App;
