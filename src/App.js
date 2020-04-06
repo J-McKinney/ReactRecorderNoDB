f47a383a04cdfd7ec7d83b585a37e11d15f5442a
Jesse McKinney <jessman51386@gmail.com>
2020-04-05 20:54:27 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Recorder from "./pages/Recorder/Recorder";
+import Dictaphone from "./pages/SpeechRecognition/Dictaphone";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Recorder />
+      <Dictaphone />
     </>
   );
 }
